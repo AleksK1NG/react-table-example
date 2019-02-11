2e9ded05128015a6b30dd83369e22d8cb5eafc2c
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-02-11 14:16:05 +0300

    add detail view layout

--- src/components/DetailRowView/DetailRowView.js
+++ src/components/DetailRowView/DetailRowView.js
@@ -3,8 +3,17 @@ import React from 'react'
 const DetailRowView = ({ row }) => {
   return (
     <div>
-      <h2>DetailRowView</h2>
-      {row && JSON.stringify(row, null, 2)}
+      <p>Выбран пользователь <b>{row.firstName + ' ' + row.lastName}</b></p>
+      <p>
+        Описание: <br/>
+        <textarea defaultValue={row.description}/>
+      </p>
+
+      <p>Адрес проживания: <b>{row.address.streetAddress}</b></p>
+      <p>Город: <b>{row.address.city}</b></p>
+      <p>Провинция/штат: <b>{row.address.state}</b></p>
+      <p>Индекс: <b>{row.address.zip}</b></p>
+
     </div>
   )
 }
